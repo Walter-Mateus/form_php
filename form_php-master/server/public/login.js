8ac3b7eb4cfcd6858c2781c8fb228dd93155c31e
Slysparrow88 <pavlusha198806@ggmail.com>
2019-11-19 21:44:03 +0200

    new

---
+++ form_php-master/server/public/login.js
@@ -0,0 +1,79 @@
+'use strict'
+
+
+var userName = document.querySelector('#username');
+var userNameError = document.querySelector('#username + .auth__error');
+ 
+  
+
+ 
+var userEmail = document.querySelector('#useremail');
+var userEmailError = document.querySelector('#useremail + .auth__error');
+
+var userPhone = document.querySelector('#userphone');
+var userPhoneError = document.querySelector('#userphone + .auth__error');
+
+var userPass = document.querySelector('#userpass');
+var userSubscribe = document.querySelector('#usersubscribe');
+
+var sendBtn = document.querySelector('#sendbtn');
+
+ 
+sendBtn.onclick = send;
+
+function send() {
+
+        var isError = false;
+
+        if(!(/^[а-яa-z0-9\-_\.]{2,25}$/i.test(userName.value))) {           
+           userNameError.classList.remove('auth__error_hide');
+           userNameError.classList.add('auth__error_show');
+                isError=true;
+        }
+
+        if(!(/^[0-9a-z.\-_]{1,15}@[0-9a-z.\-_]{1,14}\.[a-z]{1,10}$/i.test(userEmail.value))) {           
+                userEmailError.classList.remove('auth__error_hide');
+                userEmailError.classList.add('auth__error_show');
+                isError=true;
+        }
+
+        if(!(/^(\+380|380|80|0){1,4}[0-9]{9}$/.test(userPhone.value))) {           
+                userPhoneError.classList.remove('auth__error_hide');
+                userPhoneError.classList.add('auth__error_show');
+                isError=true;
+        }
+
+        var data = {
+                name: userName.value,
+                email: userEmail.value,
+                password: userPass.value,
+                subscribe: userSubscribe.value,
+
+        }
+
+        if(isError) return null;
+
+        fetch('/login', {
+                method: 'POST',
+                body: JSON.stringify(data)
+        });
+}
+
+function setClearHandler () {
+        var elements = document.querySelectorAll('.auth__text');
+
+        elements.forEach(function(element){
+        
+        element.onclick = function() {
+           console.log(this.nextElementSibling);
+
+           this.nextElementSibling.classList.remove('auth__error_show');
+           this.nextElementSibling.classList.add('auth__error_hide');
+            
+        }
+
+        });
+        
+}
+
+setClearHandler();

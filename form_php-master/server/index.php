d6b5e546faa1dfae7cb9c78199f13745b48f1fe6
Slysparrow88 <pavlusha198806@ggmail.com>
2019-12-03 20:47:40 +0200

    fresh

--- form_php-master/server/index.php
+++ form_php-master/server/index.php
@@ -1,81 +1,12 @@
-<?php 
+ <?php
+ 
+ include './handlers/validate.php';
+ 
 
 $route = $_SERVER['REQUEST_URI'];
-$method = $_SERVER ['REQUEST_METHOD'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+  include './handlers/routesMethodGet.php';
 
 
-//echo '<pre>';
-  // var_dump($method);
-//echo '</pre>';
-//die;
-
-
-
-if($method === 'GET') {
-
-
-include './views/header.php';
-
-if ($route === '/') {
-    include './views/home.php';
-}
-
-if ($route === '/contacts') {
-    include './views/contacts.php';
-}
-
-if ($route === '/login' && $method === 'GET') {
-    include './views/login.php';
-} 
-
- include './views/footer.php';
-
-}
-
-
-if ($method === 'POST') {
-    if ($route === '/login') { 
-    $request = json_decode(file_get_contents('php://input'), true);
-
-    valid($request);
-
-    echo 'hello';
-
-  }
-
-}
-
-function valid($data) 
-{
-    $errors = [];
-
-    if(!preg_match('/^[а-яa-z0-9\-_\.]{2,25}$/i', $data ['name'])) {
-        $errors['name'] = 'name is not valid';
-    }
-
-    if(!preg_match('/^[0-9a-z.\-_]{1,15}@[0-9a-z.\-_]{1,14}\.[a-z]{1,10}$/i', $data ['email'])) {
-        $errors['email'] = 'name is not valid';
-    }
-
-    if(!preg_match('/^\+380|380|80|0){1,4}[0-9]{9}$/', $data ['phone'])) {
-        $errors['phone'] = 'name is not valid';
-    }
-
-    if(!preg_match('/^[а-яa-z0-9\-_\.]{2,25}$/i', $data ['password'])) {
-        $errors['password'] = 'name is not valid';
-    }
-
-    if((!gettype($data['subscribe']) === 'boolean')) {
-
-        $errors['subscribe'] = 'name is not valid';
-
-    }
-
-    if($errors) {
-        return $errors;
-    }
-    return true;
- 
-}
-
- 
+  include './handlers/routesMethodPost.php';
